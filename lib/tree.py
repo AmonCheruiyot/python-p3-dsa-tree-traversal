d8fb3444f87ee583f7e4db3d3fa76c856adfdd9b
Amon <kiprotichamon03@gmail.com>
2024-06-24 10:11:37 +0300

    completes lab

--- lib/tree.py
+++ lib/tree.py
@@ -1,6 +1,23 @@
 class Tree:
   def __init__(self, root = None):
     self.root = root
-
+    
+  def get_element_by_id(self, id): 
+    nodes_to_visit=[self.root]
+    while nodes_to_visit:
+      node = nodes_to_visit.pop(0)
+      if node['id'] == id:
+        return node
+      if node['children']:
+        nodes_to_visit = node['children'] + nodes_to_visit
+    return None
   def get_element_by_id(self, id):
-    pass
+    nodes_to_visit=[self.root]
+    while nodes_to_visit:
+      node = nodes_to_visit.pop(0)
+      if node['id'] == id:
+        return node
+      if node['children']:
+        nodes_to_visit =nodes_to_visit + node['children']
+    return None
+    
